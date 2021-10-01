ef00796d8a03ccc90186b0d595ace728517b7bcf
Milla Dojcsák <milla.dojcsak@stud.uni-corvinus.hu>
2021-10-01 16:44:12 +0200

    hozzaadas

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -51,47 +51,48 @@ namespace UserMaintenance
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(314, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(82, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Vezetéknév";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(314, 40);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(79, 17);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Keresztnév";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(378, 9);
+            this.textBox1.Location = new System.Drawing.Point(402, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(206, 22);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(378, 40);
+            this.textBox2.Location = new System.Drawing.Point(402, 40);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(206, 22);
             this.textBox2.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(317, 84);
+            this.button1.Location = new System.Drawing.Point(317, 77);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(267, 32);
+            this.button1.Size = new System.Drawing.Size(291, 32);
             this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
+            this.button1.Text = "Hozzáadás";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(626, 450);
+            this.ClientSize = new System.Drawing.Size(705, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
