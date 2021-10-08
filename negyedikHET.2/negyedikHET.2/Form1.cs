0e63bb81d5a886ae964973db42004e3ce8e8cd73
Milla Dojcsák <milla.dojcsak@stud.uni-corvinus.hu>
2021-10-08 15:26:44 +0200

    fin

--- negyedikHET.2/negyedikHET.2/Form1.cs
+++ negyedikHET.2/negyedikHET.2/Form1.cs
@@ -19,7 +19,7 @@ namespace negyedikHET._2
         Excel.Application xlApp; // A Microsoft Excel alkalmazás
         Excel.Workbook xlWB; // A létrehozott munkafüzet
         Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
-        
+
 
         public Form1()
         {
@@ -47,7 +47,7 @@ namespace negyedikHET._2
                 xlSheet = xlWB.ActiveSheet;
 
                 // Tábla létrehozása
-                //CreateTable();
+                CreateTable();
 
                 // Control átadása a felhasználónak
                 xlApp.Visible = true;
@@ -64,6 +64,71 @@ namespace negyedikHET._2
                 xlWB = null;
                 xlApp = null;
             }
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+     "Kód",
+     "Eladó",
+     "Oldal",
+     "Kerület",
+     "Lift",
+     "Szobák száma",
+     "Alapterület (m2)",
+     "Ár (mFt)",
+     "Négyzetméter ár (Ft/m2)"};
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat fl in Flats)
+            {
+                values[counter, 0] = fl.Code;
+                values[counter, 1] = fl.Vendor;
+                values[counter, 2] = fl.Side;
+                values[counter, 3] = fl.District;
+
+                if (fl.Elevator == true) values[counter, 4] = "Van";
+                values[counter, 5] = fl.NumberOfRooms;
+                values[counter, 6] = fl.FloorArea;
+                values[counter, 7] = fl.Price;
+                values[counter, 8] = "=" + GetCell(counter + 2, 8) + "*1000000/" + GetCell(counter + 2, 7);
+                counter++;
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
