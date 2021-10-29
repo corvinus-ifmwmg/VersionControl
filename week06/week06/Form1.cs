4a4c0032dff1398579a401627a4bceb743f68c62
Milla Dojcsák <milla.dojcsak@stud.uni-corvinus.hu>
2021-10-29 09:47:59 +0200

    feldolgozas

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week06.Entities;
 using week06.MnbServiceReference;
 
@@ -21,8 +22,9 @@ namespace week06
             InitializeComponent();
             Hivas();
             dataGridView1.DataSource = Rates;
+            Feldolgozas();
         }
-        private void Hivas()
+        public string Hivas()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -35,7 +37,37 @@ namespace week06
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+            return result;
 
+        }
+
+        private void Feldolgozas()
+        {
+            // XML document létrehozása és az aktuális XML szöveg betöltése
+            var xml = new XmlDocument();
+            xml.LoadXml(Hivas());
+
+            // Végigmegünk a dokumentum fő elemének gyermekein
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
