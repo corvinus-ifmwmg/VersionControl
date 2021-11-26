b7e063784c668faa7ea778ac62ce65d6436b81d6
Milla Dojcsák <milla.dojcsak@stud.uni-corvinus.hu>
2021-11-26 22:46:28 +0100

    happypath + exception

--- week08.2/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week08.2/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -42,6 +42,53 @@ namespace UnitTestExample.Test
             var nyolchosszu = new Regex(@".{8,}");
             return kisbetu.IsMatch(password) && nagybetu.IsMatch(password) && vanszam.IsMatch(password) && nyolchosszu.IsMatch(password);
         }
+
+        [
+    Test,
+    TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+        [
+    Test,
+    TestCase("irf@uni-corvinus", "Abcd1234"),
+    TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+    TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+    TestCase("irf@uni-corvinus.hu", "Ab1234"),
+]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<Exception>(ex);
+            }
+
+            // Assert
+        }
     }
 
     
