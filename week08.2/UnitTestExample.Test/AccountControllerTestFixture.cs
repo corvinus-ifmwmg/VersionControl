bf22ecc708a1207c516a74418d519edb3014e20f
Milla Dojcsák <milla.dojcsak@stud.uni-corvinus.hu>
2021-11-26 22:41:23 +0100

    jelszovalidate

---
+++ week08.2/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test,
+        TestCase("abcd1234", false),
+    TestCase("irf@uni-corvinus", false),
+    TestCase("irf.uni-corvinus.hu", false),
+    TestCase("irf@uni-corvinus.hu", true)]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test,
+        TestCase("ABCD1234", false),
+    TestCase("Ab1234", false),
+    TestCase("Abcd1234", true),
+    TestCase("abcd1234", false),
+            TestCase("abcdABCD", false)]
+        public bool TestValidatePassword(string password)
+        {
+            var kisbetu = new Regex(@"[a-z]+");
+            var nagybetu = new Regex(@"[A-Z]+");
+            var vanszam = new Regex(@"[0-9]+");
+            var nyolchosszu = new Regex(@".{8,}");
+            return kisbetu.IsMatch(password) && nagybetu.IsMatch(password) && vanszam.IsMatch(password) && nyolchosszu.IsMatch(password);
+        }
+    }
+
+    
+    
+}
---
+++ week08.2/UnitTestExample.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b91ff6cf-1151-4584-8586-416f16cdbfec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
